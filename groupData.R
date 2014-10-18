72aeaef8d9213df3d0daaf7ab5334c944425440f
Cody Hock <codyhock74@gmail.com>
2014-10-18 12:59:18 -0400

    first round of files. Data created to model.

---
+++ groupData.R
@@ -0,0 +1,119 @@
+library(RMySQL)
+
+con <- dbConnect(MySQL(), user='cody',password='',host='localhost',dbname='NFL')
+
+#building the test model
+stats <- "select * from stats where year < 2012"
+scores<- "select * from scores where year < 2012"
+
+#building the prediction model
+predStats <- "select * from stats where year > 2011"
+predScores<- "select * from scores where year > 2011"
+
+statsDF <- dbGetQuery(con,stats)
+scoresDF <- dbGetQuery(con, scores)
+scoreResult <-dbGetQuery(con, scores)
+
+predStatsDF <- dbGetQuery(con,predStats)
+predScoresDF <- dbGetQuery(con, predScores)
+
+nrow(statsDF)
+nrow(scoresDF)
+
+allData <-as.data.frame(NULL)
+predAllData <-as.data.frame(NULL)
+#scoresDF<-scoresDF[,-5]
+
+#first renaming for the test model
+scoresDF <- rename_columns(scoresDF)
+
+#now renaming for the prediction model
+predScoresDF <- rename_columns(predScoresDF)
+
+#select scores.team1,stats.team from scores right join stats on scores.team1 = stats.team where stats.year = 2000 and scores.year = 2000;
+scoresDF <- home_away_swap(scoresDF)
+#now switching in the prediciton model
+predScoresDF <- home_away_swap(predScoresDF)
+
+
+
+##########
+#
+#make the following 2 loops into a function call
+#
+###########
+
+for(i in 1:nrow(scoresDF)){
+    scoreVector <- scoresDF[i,]
+    winVector <- dbGetQuery(con,paste(paste(paste(paste("select * from stats where team = '",scoresDF[i,2],sep=""),"' and year =",sep=""), scoresDF[i,1])," limit 1"))
+    lossVector <- dbGetQuery(con,paste(paste(paste(paste("select * from stats where team = '",scoresDF[i,4],sep=""),"' and year =",sep=""), scoresDF[i,1])," limit 1"))
+    if(length(winVector)>0 & length(lossVector)>0){
+        if(scoresDF[i,3] == '@'){
+        res <- cbind(scoreVector,lossVector,winVector)
+        }
+        else{
+        res <- cbind(scoreVector,winVector,lossVector)
+        }
+        allData <- rbind(allData,res)
+
+    }
+}
+
+for(i in 1:nrow(predScoresDF)){
+    scoreVector <- predScoresDF[i,]
+    winVector <- dbGetQuery(con,paste(paste(paste(paste("select * from stats where team = '",predScoresDF[i,2],sep=""),"' and year =",sep=""), predScoresDF[i,1])," limit 1"))
+    lossVector <- dbGetQuery(con,paste(paste(paste(paste("select * from stats where team = '",predScoresDF[i,4],sep=""),"' and year =",sep=""), predScoresDF[i,1])," limit 1"))
+    if(length(winVector)>0 & length(lossVector)>0){
+        if(predScoresDF[i,3] == '@'){
+            res <- cbind(scoreVector,lossVector,winVector)
+        }
+        else{
+            res <- cbind(scoreVector,winVector,lossVector)
+        }
+        predAllData <- rbind(predAllData,res)
+        
+    }
+}
+colnames(allData)[13] <- "Home Team"
+colnames(allData)[45] <- "Away Team"
+save(allData,file = "~/Progs/R/NFL/revisedAllData.RData")
+
+colnames(predAllData)[13] <- "Home Team"
+colnames(predAllData)[45] <- "Away Team"
+save(predAllData,file = "~/Progs/R/NFL/predictionData.RData")
+
+dbDisconnect(con)
+
+
+
+home_away_swap <- function(df){
+    for (i in 1:nrow(df)){
+        #if the away team won flip it
+        if(df[i,3] == '@'){
+            yards <- df[i,7]
+            to <- df[i,8]
+            df[i,7] = df[i,9]
+            df[i,8] = df[i,10]
+            df[i,9] <- yards
+            df[i,10] <- to
+            remove(yards)
+            remove(to)
+            df[i,11] <- df[i,6]-df[i,5]
+        }
+        else{
+            df[i,11] <- df[i,5]-df[i,6]
+        }
+    }
+    return(df)
+}
+
+rename_columns <- function(df){
+    colnames(df)[8] <- "HomeYards"
+    colnames(df)[9] <- "HomeTO"
+    colnames(df)[10] <- "AwayYards"
+    colnames(df)[11] <- "AwayTO"
+    df$Result <- 0
+    df<-df[,-2]
+    return(df)
+}
+sessionInfo()
---
+++ homeAway.R
@@ -0,0 +1,220 @@
+library(RMySQL)
+
+con <- dbConnect(MySQL(), user='cody',password='',host='localhost',dbname='NFL')
+
+scores <- "select * from scores where year < 2012"
+testScores <- "select * from scores where year > 2011"
+
+testDF <- dbGetQuery(con,testScores)
+myDF <- dbGetQuery(con,scores)
+
+dbDisconnect(con)
+
+#going through the data frame and replacing team names with -1(away) and 1(home)
+#remember, team1 won the game, team2 lost
+for(i in 1:nrow(myDF)){
+    if(myDF[i,4] == '@'){
+        myDF[i,3]=-1
+        myDF[i,5]=1
+    }
+    else{
+        myDF[i,3]=1
+        myDF[i,5]=-1        
+    }
+}
+for(i in 1:nrow(testDF)){
+    if(testDF[i,4] == '@'){
+        testDF[i,3]=-1
+        testDF[i,5]=1
+    }
+    else{
+        testDF[i,3]=1
+        testDF[i,5]=-1        
+    }
+}
+
+#without the 4th column which contains characters '@'
+myMatrix <- as.matrix(myDF[,-4])
+testMatrix <- as.matrix(testDF[,-4])
+class(testMatrix)<-"numeric"
+class(myMatrix)<-"numeric"
+nrow(myMatrix)
+
+myMatrix[267,]
+myMatrix <- cbind(myMatrix,myMatrix[,"team1"])
+testMatrix <- cbind(testMatrix,testMatrix[,"team1"])
+
+testData <- myMatrix[,7:11]
+predData <- testMatrix[,7:11]
+
+#reg(mat = testData[,-5], y = testData[,5])
+
+########
+#now building a model
+########
+testModel <- as.data.frame(testData)
+colnames(testModel)[1] <- "HomeYards"
+colnames(testModel)[2] <- "HomeTO"
+colnames(testModel)[3] <- "AwayYards"
+colnames(testModel)[4] <- "AwayTO"
+colnames(testModel)[5] <- "Result"
+
+predModel <- as.data.frame(predData)
+colnames(predModel)[1] <- "HomeYards"
+colnames(predModel)[2] <- "HomeTO"
+colnames(predModel)[3] <- "AwayYards"
+colnames(predModel)[4] <- "AwayTO"
+colnames(predModel)[5] <- "Result"
+
+
+#I need to set up the data frame to be in columns for home and away, not win and loss
+for(i in 1:nrow(testModel)){
+    #if away team won, flip the data
+    if(testModel[i,5] == -1){
+        yards <- testModel[i,1]
+        to <- testModel[i,2]
+        testModel[i,1] = testModel[i,3]
+        testModel[i,2] = testModel[i,4]
+        testModel[i,3] <- yards
+        testModel[i,4] <- to
+        remove(yards)
+        remove(to)
+    }
+}
+#again for my prediciton model
+for(i in 1:nrow(predModel)){
+    #if away team won, flip the data
+    if(predModel[i,5] == -1){
+        yards <- predModel[i,1]
+        to <- predModel[i,2]
+        predModel[i,1] = predModel[i,3]
+        predModel[i,2] = predModel[i,4]
+        predModel[i,3] <- yards
+        predModel[i,4] <- to
+        remove(yards)
+        remove(to)
+    }
+}
+
+testModel$HomeTO2 <- testModel$HomeTO^2
+testModel$AwayTO2 <- testModel$AwayTO^2
+testModel <- testModel[,-8]
+
+#Page 179
+model <- 
+    lm(Result ~ 
+           HomeYards + 
+           HomeYards*AwayYards +
+           HomeTO +
+           HomeTO*AwayYards +
+           HomeTO*HomeYards + 
+           HomeTO2 + 
+           HomeTO2*AwayYards +
+           HomeTO2*HomeYards + 
+           AwayYards + 
+           AwayTO +
+           AwayTO*AwayYards +
+           AwayTO*HomeYards +
+           AwayTO2 +
+           AwayTO2*AwayYards + 
+           AwayTO2*HomeYards, 
+       data = testModel)
+
+model
+summary(model)
+
+yardModel <- lm(Result ~ HomeYards + AwayYards , data = testModel)
+yardModel
+summary(yardModel)
+
+toModel <- lm(Result ~ HomeTO + AwayTO , data = testModel)
+toModel
+summary(toModel)
+
+
+plot(model)
+
+
+predModel$HomeTO2 <- predModel$HomeTO^2
+predModel$AwayTO2 <- predModel$AwayTO^2
+#predict outcomes
+pred  <- predict(model, predModel)
+predModel
+summary(pred)
+plot(pred,predModel$Result,main="Home (1) VS Away (-1)",ylab="Actual",xlab="Predicted")
+correct <-table(predModel$Result,pred)
+barplot(correct)
+length(pred)
+
+hist(pred)
+
+save(pred,file="HomeAwayPrediction.RData")
+
+#
+#
+pred
+
+
+
+
+
+#number of games played is 3186
+#Home team wins = 1826
+#Away team wins = 1360
+
+#colSums(myMatrix)
+#myMatrix[1,2]
+#homeWins <- 0
+#nrow(myMatrix)
+#for (i in 1:nrow(myMatrix)){
+#    if(myMatrix[i,3] == 1){
+#        homeWins <- homeWins + 1
+#    }
+#}
+#homeWins
+
+
+
+
+
+
+
+
+
+
+
+
+#regression function
+#page 168-171
+#y = b0 + b1x1 + b2x2 +...+ bixi + eps
+#Y = XB + eps
+#B = (X^t X)^-1 X^t Y
+reg <- function(mat,y){
+    #creates new column and fills with 1
+    mat <- cbind (Intercept = 1, mat)
+    # solve() is the inverse of matrix
+    # t() is transpose
+    # %*% is matrix multiplication 
+    solve (t(mat) %*% mat) %*% t(mat) %*% y
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
